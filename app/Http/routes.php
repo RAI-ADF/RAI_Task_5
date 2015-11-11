6a79921f838b1f54c2ad90f0d8eae2136a73dfb6
Aulia Marchita Ramadhani <auliamarchita@gmail.com>
2015-11-11 19:34:48 +0700

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,8 @@
 <?php
+use App\Anggota;
+use App\Status;
+use App\TokoBuku;
+use App\Buku;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +18,45 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('relasi-1', function() {
+
+		$anggota = Anggota::where('email', '=', 'auliamarchita@gmail.com')->first();
+		return $anggota->status->pekerjaan;
+
+});
+
+Route::get('relasi-2', function() {
+
+	
+		$anggota = Anggota::where('email', '=', 'auliamarchita@gmail.com')->first();
+		return $anggota->tokoBuku->nama;
+
+	});
+
+Route::get('relasi-3', function() {
+
+
+		$tokoBuku = TokoBuku::where('nama', '=', 'Taman Bacaan')->first();
+		foreach ($tokoBuku->anggota as $temp)
+			echo '<li>  Nama : ' . $temp->nama . '
+					    ' . $temp->email . ' </li>';
+	});
+
+Route::get('relasi-4', function() {
+
+
+		$aulia = Anggota::where('nama', '=', 'Aulia Marchita')->first();
+		foreach ($aulia->buku as $temp) 
+			echo '<li>' . $temp->judul . '</li>';
+	});
+
+
+Route::get('relasi-5', function() {
+
+
+		$circa = Buku::where('judul', '=', 'Circa')->first();
+		foreach ($circa->anggota as $temp)
+			echo '<li> Nama : ' . $temp->nama . ' <strong>' . $temp->email . '</strong></li>';
+
+	});
