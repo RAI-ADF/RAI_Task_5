ec1a20b5e2e62749062eae2949cf2dac9acdfab1
rizkidoank <rizki.doank94@gmail.com>
2015-12-11 19:53:19 +0700

    finish

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <title>Go-Laundry</title>
+    <title>RAI_TASK_5</title>
     <meta charset="UTF-8">
     <meta name=description content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
